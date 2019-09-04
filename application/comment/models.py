08075c445e2e54059da0cd1b8435d12edf958e91
Ahti Katiska <ahti.katiska@gmail.com>
2019-09-04 16:27:35 +0300

    Update ERD, update TODO

--- application/comment/models.py
+++ application/comment/models.py
@@ -1,24 +1,16 @@
 from application import db
+from application.models import Base
 
 
-class Comment(db.Model):
+class Comment(Base):
 
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-                              onupdate=db.func.current_timestamp())
-
-    author_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('account.id'),
+                          nullable=False)
     author = db.relationship("User", backref="comments_created", lazy=True)
 
     thread_id = db.Column(db.Integer, db.ForeignKey('thread.id'),
                           nullable=False)
     thread = db.relationship("Thread", backref="thread_comments", lazy=True)
-
-    parent_comment_id = db.Column(db.Integer, db.ForeignKey('comment.id'),
-                                  nullable=True)
-    comments = db.relationship("Comment",
-                               backref=db.backref('parent', remote_side=[id]))
 
     content = db.Column(db.Text, nullable=False)
 
--- application/threads/models.py
+++ application/threads/models.py
@@ -1,14 +1,14 @@
 from application import db
+from application.models import Base
 
 
-class Thread(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+class Thread(Base):
     title = db.Column(db.String(144), nullable=False)
     locked = db.Column(db.Boolean, default=False, nullable=False)
     content = db.Column(db.Text, nullable=False)
 
-    author_id = db.Column(db.Integer, db.ForeignKey('account.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey('account.id'),
+                          nullable=False)
     author = db.relationship("User", backref="threads_created")
 
     def __init__(self, title, content, auth_id):
